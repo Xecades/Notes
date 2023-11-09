8d01bd26f189f37e5e29ac624228b12f96e36a4e
Xecades <i@xecades.xyz>
2023-11-09 23:47:16 +0800

    feat: add `charcount.sh` that counts the number of characters in a folder

---
+++ charcount.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Usage: charcount [path] [extension]
+# Description: Count the number of characters in a file or files in a directory
+# Example: charcount ./docs txt
+
+cnt=0
+path=$1
+ext=$2
+
+__count_path__() {
+    if [[ -f $1 ]]; then
+        if [[ $1 == *.$2 || -z $ext ]]; then
+            cnt=$((cnt + $(wc -m < "$1" 2> /dev/null)))
+        fi
+        return
+    elif [[ -d $1 ]]; then
+        for f in "$1"/*; do
+            __count_path__ "$f" "$2"
+        done
+    fi
+}
+
+if [[ -z $path ]]; then
+    path="$(pwd)"
+fi
+
+if [[ ! -e $path ]]; then
+    echo "Error: $path does not exist"
+    exit 1
+fi
+
+__count_path__ "$path" "$ext"
+echo $cnt
